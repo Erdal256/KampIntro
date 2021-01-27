9a7b07035f237f9fc43182d960f4d6eb9335d896
Erdal256 <erdall.2693@gmail.com>
2021-01-28 00:24:23 +0300

    27 ocak

---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if(gamer.BirthYear=="1996" && gamer.FirstName == "ERDAL" && gamer.LastName =="SARIŞEN" &&gamer.IdentityNumber==12345)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
